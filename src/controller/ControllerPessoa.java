b2f13f60d423ff55f9ab60ff9b0b64ed583ca825
Guilherme Machado da Silva <150199325+GuiXado@users.noreply.github.com>
2025-11-12 01:25:40 -0300

    Refactor search and delete methods in ControllerPessoa

--- src/controller/ControllerPessoa.java
+++ src/controller/ControllerPessoa.java
@@ -24,31 +24,30 @@ public class ControllerPessoa {
 		vetor[posicao].addFirst(p);
     }
 
-    public Pessoa buscar(String nomeConsulta) throws Exception {
+	public void Mostrarbusca(String nomeConsulta) throws Exception {
+    	int[] valores = buscar(nomeConsulta);
+    	Pessoa p = (Pessoa) vetor[valores[0]].get(valores[1]);
+    	System.out.println(p.toString());
+    }
+
+    public void excluir(String pessoaRemove) throws Exception {
+    	int[] valores = buscar(pessoaRemove);
+        vetor[valores[0]].remove(valores[1]);
+    }
+    
+    private int[] buscar(String nomeConsulta) throws Exception {
     	for (int i = 0; i < vetor.length; i++) {
             int tamanho = vetor[i].size();
             for (int j = 0; j < tamanho; j++) {
                 Pessoa p = (Pessoa) vetor[i].get(j);
                 if (p.nome.equalsIgnoreCase(nomeConsulta)) {
-                    return p;
+                	return new int[]{i, j};
                 }
             }
         }
         throw new Exception("Pessoa não encontrada");
     }
 
-    public void excluir(String pessoaRemove) throws Exception {
-    	for (int i = 0; i < vetor.length; i++) {
-            int tamanho = vetor[i].size();
-            for (int j = 0; j < tamanho; j++) {
-                Pessoa p = (Pessoa) vetor[i].get(j);
-                if (p.nome.equalsIgnoreCase(pessoaRemove)) {
-                    vetor[i].remove(j);
-                    return;
-                }
-            }
-        }
-        throw new Exception("Pessoa não encontrada para exclusão");
-    }
     
 }
+
